5dd1ab00b920c1a9f24a40555310ed6a01ed61a0
Allison <124654013+dyolf-walrus@users.noreply.github.com>
2023-04-30 02:06:22 -0400

    update index.html. complete scripts.js.

--- scripts.js
+++ scripts.js
@@ -39,13 +39,23 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    let playerWins;
-    let computerWins;
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playRound(playerSelection, computerSelection))
+    let playerWins = 0;
+    let computerWins = 0;
+    for (i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice()
+        let playerSelection = prompt('Choose rock, paper, or scissors. You will be prompted 5 times for 5 rounds!')
+        playRound(playerSelection, computerSelection)
+        if (playRound(playerSelection, computerSelection).includes('win')) {
+            playerWins++;
+        } else if (playRound(playerSelection, computerSelection).includes('lose')) {
+            computerWins++;
+        }
+    }
+    if (playerWins > computerWins) {
+        return `You won ${playerWins} rounds and the computer won ${computerWins}. You win the game!`;
+    } else if (playerWins < computerWins) {
+        return `You won ${playerWins} rounds and the computer won ${computerWins}. You lose the game!`;
+    } else return `The game was a tie!`
 }
 
 const playerSelection = 'rock';
